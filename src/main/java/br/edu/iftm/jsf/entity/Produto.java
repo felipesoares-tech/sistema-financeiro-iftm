aaf75c313e0907bf4a0e0bb90b914e39876ab466
Felipe Soares <soares.felipe200@gmail.com>
2023-07-01 11:21:21 -0300

    corrigindo bug fornecedor

--- src/main/java/br/edu/iftm/jsf/entity/Produto.java
+++ src/main/java/br/edu/iftm/jsf/entity/Produto.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -33,6 +36,9 @@ public class Produto implements Serializable{
     private Date dataFabricacao;
     @Column(name = "foto_banco")
     private byte fotoBanco;
+    @OneToOne
     private Fornecedor fornecedor;
+    @OneToOne
+    private Marca marca;
     
 }
--- src/main/java/br/edu/iftm/jsf/logic/ProdutoLogic.java
+++ src/main/java/br/edu/iftm/jsf/logic/ProdutoLogic.java
@@ -10,38 +10,44 @@ import javax.inject.Inject;
 
 public class ProdutoLogic implements GenericLogic<Produto> {
 
-
     @Inject
     private ProdutoDAO dao;
-    
+
     @Override
     @Transacional
-    public Produto salvar(Produto entity)  throws ErroNegocioException, ErroSistemaException{
-        if("".equals(entity.getNome())){
+    public Produto salvar(Produto entity) throws ErroNegocioException, ErroSistemaException {
+        if ("".equals(entity.getNome())) {
             throw new ErroNegocioException("Por favor informe o nome");
         }
-        if("".equals(entity.getDescricao())){
+        if ("".equals(entity.getDescricao())) {
             throw new ErroNegocioException("Por favor informe a descrição");
         }
-        if(entity.getValor() == null || entity.getValor().floatValue() <= 0f){
+        if (entity.getValor() == null || entity.getValor().floatValue() <= 0f) {
             throw new ErroNegocioException("Por favor informe um valor maior que zero.");
         }
-        if(entity.getDataFabricacao() == null){
-            throw new ErroNegocioException("Por favor informe a data fabricação");
+        if (entity.getDataFabricacao() == null) {
+            throw new ErroNegocioException("Por favor informe a data de fabricação");
         }
+        if (entity.getFornecedor() == null || entity.getFornecedor().getId() == null) {
+            throw new ErroNegocioException("Por favor selecione um fornecedor");
+        }
+        if (entity.getMarca() == null || entity.getMarca().getId() == null) {
+            throw new ErroNegocioException("Por favor selecione uma marca");
+        }
+
         entity = dao.salvar(entity);
         return entity;
     }
 
     @Override
     @Transacional
-    public void remover(Produto entity)  throws ErroNegocioException, ErroSistemaException{
+    public void remover(Produto entity) throws ErroNegocioException, ErroSistemaException {
         dao.deletar(entity.getId());
     }
 
     @Override
-    public List<Produto> listar()  throws ErroNegocioException, ErroSistemaException{
+    public List<Produto> listar() throws ErroNegocioException, ErroSistemaException {
         return dao.listar();
     }
-    
+
 }
