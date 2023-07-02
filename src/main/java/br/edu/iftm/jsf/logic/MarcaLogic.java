a5c46285d52f27737f23db1a299437b0ff5226b5
Felipe Soares <soares.felipe200@gmail.com>
2023-07-02 11:08:47 -0300

    adicionando filtro para metodo de auto complete

--- src/main/java/br/edu/iftm/jsf/logic/MarcaLogic.java
+++ src/main/java/br/edu/iftm/jsf/logic/MarcaLogic.java
@@ -5,24 +5,20 @@ import br.edu.iftm.jsf.entity.Marca;
 import br.edu.iftm.jsf.util.Transacional;
 import br.edu.iftm.jsf.util.exception.ErroNegocioException;
 import br.edu.iftm.jsf.util.exception.ErroSistemaException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.inject.Inject;
 
-public class MarcaLogic implements GenericLogic<Marca>{
+public class MarcaLogic implements GenericLogic<Marca> {
 
     @Inject
     private MarcaDAO dao;
-    
-    
-    
-    public List<Marca> listarTodas(){
-        return dao.listar();
-    }
 
     @Override
     @Transacional
     public Marca salvar(Marca entity) throws ErroNegocioException, ErroSistemaException {
-       throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
@@ -34,5 +30,17 @@ public class MarcaLogic implements GenericLogic<Marca>{
     public List<Marca> listar() throws ErroNegocioException, ErroSistemaException {
         return dao.listar();
     }
-    
+
+    public List<Marca> pesquisaLike(String texto) {
+        List<Marca> marcasFiltradas = new ArrayList<>();
+        Iterator<Marca> it = dao.listar().iterator();
+        while (it.hasNext()) {
+            Marca marca = (Marca) it.next();
+            if (marca.getNome().toLowerCase().contains(texto.toLowerCase())) {
+                marcasFiltradas.add(marca);
+            }
+        }
+        return marcasFiltradas;
+    }
+
 }
